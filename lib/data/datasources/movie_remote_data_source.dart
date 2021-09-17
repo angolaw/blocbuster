c5026edc6f82ac18a369cbfec260a760b8c90a63
Willian S <wsapcf@gmail.com>
2021-09-17 09:45:13 -0300

    add getTrending implementation on concrete MovieRemoteDataSource class

--- lib/data/datasources/movie_remote_data_source.dart
+++ lib/data/datasources/movie_remote_data_source.dart
@@ -1,13 +1,23 @@
+import 'package:blocbuster/data/core/api_constants.dart';
 import 'package:blocbuster/data/models/movie_model.dart';
+import 'package:http/http.dart';
 
 abstract class MovieRemoteDataSource {
   Future<List<MovieModel>> getTrending();
 }
 
 class MovieRemoteDataSourceImpl extends MovieRemoteDataSource {
+  final Client _client;
+
+  MovieRemoteDataSourceImpl(this._client);
+
   @override
-  Future<List<MovieModel>> getTrending() {
-    // TODO: implement getTrending
-    throw UnimplementedError();
+  Future<List<MovieModel>> getTrending() async {
+    final response = await _client.get(
+        Uri.parse(
+            '${ApiConstants.BASE_URL}trending/movie/day?api_key=${ApiConstants.API_KEY}'),
+        headers: {
+          'Content-Type': 'application/json',
+        });
   }
 }
