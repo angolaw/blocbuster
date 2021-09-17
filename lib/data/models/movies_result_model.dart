be46122cb2077e96e86afb78fc2675d500b1a8b5
Willian S <wsapcf@gmail.com>
2021-09-17 09:27:01 -0300

    add MovieModel to data layer

--- lib/data/models/movies_result_model.dart
+++ lib/data/models/movies_result_model.dart
@@ -1,11 +1,7 @@
 class MoviesResultModel {
-  int page;
   List<Results> results;
-  int totalPages;
-  int totalResults;
 
-  MoviesResultModel(
-      {this.page, this.results, this.totalPages, this.totalResults});
+  MoviesResultModel({this.results});
 
   MoviesResultModel.fromJson(Map<String, dynamic> json) {
     page = json['page'];
@@ -15,91 +11,14 @@ class MoviesResultModel {
         results.add(new Results.fromJson(v));
       });
     }
-    totalPages = json['total_pages'];
-    totalResults = json['total_results'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['page'] = this.page;
     if (this.results != null) {
       data['results'] = this.results.map((v) => v.toJson()).toList();
     }
-    data['total_pages'] = this.totalPages;
-    data['total_results'] = this.totalResults;
-    return data;
-  }
-}
 
-class Results {
-  bool adult;
-  String backdropPath;
-  List<int> genreIds;
-  int id;
-  String originalLanguage;
-  String originalTitle;
-  String overview;
-  String posterPath;
-  String releaseDate;
-  String title;
-  bool video;
-  double voteAverage;
-  int voteCount;
-  double popularity;
-  String mediaType;
-
-  Results(
-      {this.adult,
-      this.backdropPath,
-      this.genreIds,
-      this.id,
-      this.originalLanguage,
-      this.originalTitle,
-      this.overview,
-      this.posterPath,
-      this.releaseDate,
-      this.title,
-      this.video,
-      this.voteAverage,
-      this.voteCount,
-      this.popularity,
-      this.mediaType});
-
-  Results.fromJson(Map<String, dynamic> json) {
-    adult = json['adult'];
-    backdropPath = json['backdrop_path'];
-    genreIds = json['genre_ids'].cast<int>();
-    id = json['id'];
-    originalLanguage = json['original_language'];
-    originalTitle = json['original_title'];
-    overview = json['overview'];
-    posterPath = json['poster_path'];
-    releaseDate = json['release_date'];
-    title = json['title'];
-    video = json['video'];
-    voteAverage = json['vote_average'];
-    voteCount = json['vote_count'];
-    popularity = json['popularity'];
-    mediaType = json['media_type'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['adult'] = this.adult;
-    data['backdrop_path'] = this.backdropPath;
-    data['genre_ids'] = this.genreIds;
-    data['id'] = this.id;
-    data['original_language'] = this.originalLanguage;
-    data['original_title'] = this.originalTitle;
-    data['overview'] = this.overview;
-    data['poster_path'] = this.posterPath;
-    data['release_date'] = this.releaseDate;
-    data['title'] = this.title;
-    data['video'] = this.video;
-    data['vote_average'] = this.voteAverage;
-    data['vote_count'] = this.voteCount;
-    data['popularity'] = this.popularity;
-    data['media_type'] = this.mediaType;
     return data;
   }
 }
