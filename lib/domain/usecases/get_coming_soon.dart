a2eefca18011d38db861ba5918731059026e158f
Willian S <wsapcf@gmail.com>
2021-09-17 11:15:10 -0300

    add UseCase extension on GetComingSoon

--- lib/domain/usecases/get_coming_soon.dart
+++ lib/domain/usecases/get_coming_soon.dart
@@ -1,13 +1,16 @@
 import 'package:blocbuster/domain/entities/app_error.dart';
 import 'package:blocbuster/domain/entities/movie_entity.dart';
+import 'package:blocbuster/domain/entities/no_params.dart';
 import 'package:blocbuster/domain/repositories/movie_repository.dart';
+import 'package:blocbuster/domain/usecases/usecase.dart';
 import 'package:dartz/dartz.dart';
 
-class GetComingSoon {
+class GetComingSoon extends UseCase<List<MovieEntity>, NoParams> {
   final MovieRepository repository;
   GetComingSoon(this.repository);
 
-  Future<Either<AppError, List<MovieEntity>>> call() async {
+  @override
+  Future<Either<AppError, List<MovieEntity>>> call(NoParams noParams) async {
     return await repository.getComingSoon();
   }
 }
