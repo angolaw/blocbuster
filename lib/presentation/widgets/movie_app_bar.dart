ddd7907bcaa73f12b08366e7c72c220d7fc50b47
Willian S <wsapcf@gmail.com>
2021-09-18 11:17:03 -0300

    add MovieAppBar with Logo

---
+++ lib/presentation/widgets/movie_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:blocbuster/common/constants/sizes.dart';
+import 'package:blocbuster/common/screenutil/screen_util.dart';
+import 'package:blocbuster/presentation/widgets/logo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../common/extensions/size_extension.dart';
+
+class MovieAppBar extends StatelessWidget {
+  const MovieAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: ScreenUtil().statusBarHeight + Sizes.dimen_4.h,
+        left: Sizes.dimen_16.w.toDouble(),
+        right: Sizes.dimen_16.w.toDouble(),
+      ),
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('assets/svgs/menu.svg',
+                height: Sizes.dimen_12.h.toDouble()),
+          ),
+          const Expanded(
+            child: Logo(height: Sizes.dimen_14),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search,
+                color: Colors.white, size: Sizes.dimen_12.h.toDouble()),
+          )
+        ],
+      ),
+    );
+  }
+}
