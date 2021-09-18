827d1031330227e759f7acf86443ff6ef37c1d46
Willian S <wsapcf@gmail.com>
2021-09-18 11:27:01 -0300

    convert MoviePageView to stateful and add PageController

--- lib/presentation/journeys/home/movie_carousel/movie_page_view.dart
+++ lib/presentation/journeys/home/movie_carousel/movie_page_view.dart
@@ -1,12 +1,30 @@
 import 'package:blocbuster/domain/entities/movie_entity.dart';
 import 'package:flutter/material.dart';
 
-class MoviePageView extends StatelessWidget {
+class MoviePageView extends StatefulWidget {
   final List<MovieEntity> movies;
   final int initialPage;
   const MoviePageView(
       {Key? key, required this.movies, required this.initialPage})
       : super(key: key);
+
+  @override
+  State<MoviePageView> createState() => _MoviePageViewState();
+}
+
+class _MoviePageViewState extends State<MoviePageView> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController(
+      initialPage: widget.initialPage,
+      keepPage: false,
+      viewportFraction: 0.7,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
