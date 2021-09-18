4e1ca61b443409456c5f53d062194b3f0c186eb6
Willian S <wsapcf@gmail.com>
2021-09-18 11:19:28 -0300

    update HomeScreen with BlocProvider and BlocBuilder

--- lib/presentation/journeys/home_screen.dart
+++ lib/presentation/journeys/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:blocbuster/di/get_it.dart';
 import 'package:blocbuster/presentation/blocs/movie_carousel/movie_carousel_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -28,21 +29,31 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: const [
-          FractionallySizedBox(
-            alignment: Alignment.topCenter,
-            heightFactor: 0.6,
-            child: Placeholder(color: Colors.red),
-          ),
-          FractionallySizedBox(
-            alignment: Alignment.bottomCenter,
-            heightFactor: 0.4,
-            child: Placeholder(color: Colors.blue),
-          ),
-        ],
+    return BlocProvider(
+      create: (_) => movieCarouselBloc,
+      child: Scaffold(
+        body: BlocBuilder<MovieCarouselBloc, MovieCarouselState>(
+          builder: (context, state) {
+            if (state is MovieCarouselLoaded) {
+              return Stack(
+                fit: StackFit.expand,
+                children: const [
+                  FractionallySizedBox(
+                    alignment: Alignment.topCenter,
+                    heightFactor: 0.6,
+                    child: Placeholder(color: Colors.red),
+                  ),
+                  FractionallySizedBox(
+                    alignment: Alignment.bottomCenter,
+                    heightFactor: 0.4,
+                    child: Placeholder(color: Colors.blue),
+                  ),
+                ],
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
