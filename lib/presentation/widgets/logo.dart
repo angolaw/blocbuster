55caa133caad123513df095221e99ca86bf20deb
Willian S <wsapcf@gmail.com>
2021-09-18 11:12:30 -0300

    add Logo widget

---
+++ lib/presentation/widgets/logo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../../common/extensions/size_extension.dart';
+
+class Logo extends StatelessWidget {
+  final double height;
+  const Logo({Key? key, required this.height}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/pngs/logo.png',
+        color: Colors.white, height: height.h.toDouble());
+  }
+}
